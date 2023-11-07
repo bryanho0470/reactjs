045eebf5d82a232b462fc87c8945a93e47545b28
bryanho0470 <bryanho0470@gmail.com>
2023-11-08 06:05:42 +0900

    how to code when useEffect destroyed.

--- react-for-beginer/src/App.js
+++ react-for-beginer/src/App.js
@@ -1,36 +1,21 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => {
-    return setValue((event) => event + 1);
-  };
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time");
+function Hello() {
   useEffect(() => {
-    console.log("I run only once");
+    console.log("Created :)");
+    return () => console.log("Destroyed :(");
   }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("Searching for", keyword);
-    }
-  }, [keyword]);
+  return <h1>Hello!</h1>;
+}
 
-  useEffect(() => {
-    console.log("This will change when counter changed");
-  }, [counter]);
+function App() {
+  const [status, setStatus] = useState(false);
+  const onClick = () => setStatus((prev) => !prev);
 
   return (
     <div>
-      <input
-        vlaue={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click ME!</button>
+      {status ? <Hello /> : null}
+      <button onClick={onClick}>{status ? "Hide" : "Show"}</button>
     </div>
   );
 }
