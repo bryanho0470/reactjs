882fff61e56556ede07d7f04ec616676173e11d2
bryanho0470 <bryanho0470@gmail.com>
2023-11-08 05:40:07 +0900

    useEffect watching variable

--- react-for-beginer/src/App.js
+++ react-for-beginer/src/App.js
@@ -1,11 +1,36 @@
-import Button from "./button.js";
-import styles from "./app.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    return setValue((event) => event + 1);
+  };
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Searching for", keyword);
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("This will change when counter changed");
+  }, [counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>welcome back!</h1>
-      <Button text={"Click me!!"} />
+      <input
+        vlaue={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click ME!</button>
     </div>
   );
 }
